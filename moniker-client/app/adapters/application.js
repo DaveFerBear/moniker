2797226b5bcc8d628843f1be4defce32d7a8909a
David Ferris <david.j.ferris.21@gmail.com>
2016-11-01 21:29:50 -0400

    remove pluralization

--- moniker-client/app/adapters/application.js
+++ moniker-client/app/adapters/application.js
@@ -2,5 +2,7 @@ import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
     host: 'http://ec2-54-191-92-106.us-west-2.compute.amazonaws.com',
-    namespace: 'getMoniker'
+    pathForType: function(type) {
+        return type;
+    },
 });
