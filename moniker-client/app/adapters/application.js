d2fc2328c9fade5d190b72e37499e44b99221c22
David Ferris <david.j.ferris.21@gmail.com>
2016-11-06 12:22:40 -0500

    404 transition

--- moniker-client/app/adapters/application.js
+++ moniker-client/app/adapters/application.js
@@ -1,8 +1,9 @@
 import DS from 'ember-data';
 
-export default DS.JSONAPIAdapter.extend({
+export default DS.RESTAdapter.extend({
     host: 'http://ec2-54-191-92-106.us-west-2.compute.amazonaws.com',
-    pathForType: function(type) {
+
+    pathForType: function(type) { //removes pluralization, can also be used for camelCase
         return type;
     },
 });
