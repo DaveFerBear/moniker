a13828e3a4a4b5e56311458d1cba62ba20d7dbfb
David Ferris <david.j.ferris.21@gmail.com>
2016-11-09 00:14:43 -0500

    animtion

--- moniker-client/app/components/network-input.js
+++ moniker-client/app/components/network-input.js
@@ -4,7 +4,8 @@ export default Ember.Component.extend({
     inputVal: "",
     actions: {
         keyUp: function() {
-            this.sendAction('action', this.get('attr.name'), this.inputVal); //send name and input value
+            console.log('eg');
+            //this.sendAction('action', this.get('attr.name'), this.inputVal); //send name and input value
         }
     }
 });
