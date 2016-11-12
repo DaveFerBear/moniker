80fe10640907979af4807cba4ff25de148483f6d
David Ferris <david.j.ferris.21@gmail.com>
2016-11-12 12:57:04 -0500

    fix errors

--- moniker-client/app/components/network-input.js
+++ moniker-client/app/components/network-input.js
@@ -4,8 +4,8 @@ export default Ember.Component.extend({
     inputVal: "",
     actions: {
         keyUp: function() {
-            console.log('eg');
-            //this.sendAction('action', this.get('attr.name'), this.inputVal); //send name and input value
+            //TODO: validate URL
+            this.sendAction('action', this.get('attr.name'), this.inputVal); //send name and input value
         }
     }
 });
--- moniker-client/app/routes/application.js
+++ moniker-client/app/routes/application.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
-        console.log(this.store.findRecord('profile', 'test'));
+        //console.log(this.store.findRecord('profile', 'test'));
         return;
     },
 
